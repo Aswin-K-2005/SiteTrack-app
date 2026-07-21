e053da02854220546f11d98d4f79dee835e7cc0d
Aswin Kumar <aswin.kss2005@gmail.com>
2026-07-21 23:40:38 +0530

    fixesss

--- frontend/public/firebase-messaging-sw.js
+++ frontend/public/firebase-messaging-sw.js
@@ -1,21 +1,21 @@
 importScripts('https://www.gstatic.com/firebasejs/10.12.2/firebase-app-compat.js');
 importScripts('https://www.gstatic.com/firebasejs/10.12.2/firebase-messaging-compat.js');
 
-// 1. Initialize Firebase exactly as you did in firebase.js
-const firebaseConfig = {
+// 1. Initialize Firebase App (Using the 'compat' syntax that Service Workers require)
+firebase.initializeApp({
   apiKey: "AIzaSyCMnQZNUOVbo8R5OqutkzIhOcNkZ3dewTE",
   authDomain: "sitetrack-backend.firebaseapp.com",
   projectId: "sitetrack-backend",
   storageBucket: "sitetrack-backend.firebasestorage.app",
   messagingSenderId: "580393625486",
-  appId: "1:580393625486:web:f46229e8ec7ced30ee66df",
-  measurementId: "G-QSWSDNRMF8"
-};
+  appId: "1:580393625486:web:f46229e8ec7ced30ee66df"
+});
 
 // 2. Initialize Messaging
 const messaging = firebase.messaging();
 
 // 3. THIS IS THE MISSING PIECE: Tell the SW how to decrypt the message
+// REPLACE THE VAPID KEY BELOW WITH YOUR NEW FIREBASE VAPID KEY!
 messaging.getToken({ 
   vapidKey: "BL59yeNy8YSJvtYDHFir6N32lB3TyIgIXi76iOXvq-dobpBeKzjIySgCwvLRFLzJd5n_qMNuf_hD8uNRylj5jJw" 
 });
